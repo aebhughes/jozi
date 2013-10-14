501568a2d4f08e96fc77cb6552df7ca9098d1b05
Adrian Hughes <Adrianhughes@Adrian-Hughess-MacBook.local>
2013-10-14 20:55:27 +0200

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post  
+import datetime
+
+def index(request, ):
+	# get the blog posts that are published
+	cdate = datetime.date.today() - datetime.timedelta(days=1)
+	posts = Post.objects.filter(published = True, exDate__gt= cdate).order_by('exDate')
+	# now return the rendered template
+	return render(request, 'blog/index.html', {'posts':posts})
+
+def post(request, slug):
+	# get the Post object
+	post = get_object_or_404(Post, slug=slug)
+	# now return the rendered template
+	return render(request, 'blog/post.html', {'post':post})
+
---
+++ jozi/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+)
